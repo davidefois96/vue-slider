b5ee83fe25ff9f844fdfc0e1c2f59a9ce8349dba
davidefois96 <davidefois360@gmail.com>
2024-03-12 17:52:22 +0100

    [add] new code

--- JS/script.js
+++ JS/script.js
@@ -15,38 +15,39 @@ createApp({
         { 
         url :'https://www.sardegnaturismo.it/sites/default/files/styles/larghezza_contenitore/public/galleria/torre_di_portoscuso_trolvag_comm_wiki.jpg?itok=iKOkXowb' ,
         nome : 'Portoscuso',
-        descrizione :'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+        descrizione :'Piccolo paese costiero nel sudovest della Sardegna'
 
         }, 
         { 
         url :'https://www.sardegna.info/wp-content/uploads/sites/5/portoscuso.jpg' ,
         nome : 'Lungomare Cristoforo Colombo',
-        descrizione :'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+        descrizione :'Lungomare situato nel centro del paese'
 
         }, 
         { 
           
         url :'https://www.radiox.it/wp-content/uploads/tonnaraportoscuso.jpg' ,
         nome : 'Tonnara',
-        descrizione :'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+        descrizione :'Antica tonnara storica di Portoscuso'
 
         }, 
         { 
         url :'https://www.sacruxitta.it/wp-content/uploads/2012/07/pano-da-scalette.jpg',
         nome : 'Portopaglietto',
-        descrizione :'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+        descrizione :'Piccola spiaggia nella periferia del paese'
 
         }, 
         { 
         url :'https://i.ytimg.com/vi/HADEzZGUz0c/hq720.jpg?sqp=-oaymwE7CK4FEIIDSFryq4qpAy0IARUAAAAAGAElAADIQj0AgKJD8AEB-AH-CYAC0AWKAgwIABABGCUgYShlMA8=&amp;rs=AOn4CLBmGaElfP-VSCUtrxQ2bCOP_mh6_g',
         nome: 'Capoaltano',
-        descrizione :'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+        descrizione :'Parte di costa incontaminata situata esternamente al paese'
 
         }, 
 
       ],
       cont: 0,
-      activeAutoScroll: true
+      activeAutoScroll: true,
+      towardAutoScroll: true,
 
     }
 
@@ -67,17 +68,19 @@ createApp({
         this.cont=this.images.length - 1;
       }
 
+     
     },
     
     
     autoScroll(){
 
-    
+      
       setInterval( ()=> {
       
         if(this.activeAutoScroll){
 
-          this.scrollImg(true)
+          this.towardAutoScroll ? this.scrollImg(true) : this.scrollImg(false)
+
         }
 
       },3000)
